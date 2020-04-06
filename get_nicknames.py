be70e4eb41da1ab5725f0417be0c9c2664e78868
Viktor Sharepo <shedelok.1@gmail.com>
2020-04-06 15:21:18 +0300

    Initial commit

---
+++ get_nicknames.py
@@ -0,0 +1,30 @@
+import ndjson
+import requests
+
+from collections import defaultdict
+
+tournamentId = raw_input("Enter tournament id: ")
+minimumGames = int(raw_input("Enter minimum number of games: "))
+
+requestUrl = 'https://lichess.org/api/tournament/' + tournamentId + '/games'
+
+print("Requesting data from lichess...")
+response = requests.get(requestUrl, headers = {'Accept' : 'application/x-ndjson'}, params = {'moves' : 'false', 'tags' : 'false'})
+games = response.json(cls = ndjson.Decoder)
+
+print("Counting games...")
+gamesPlayed = defaultdict(int)
+for game in games:
+    players = game['players']
+    for color in 'white', 'black':
+        gamesPlayed[players[color]['user']['name'].encode("ascii")] += 1
+
+print("Filtering by number of games...")
+names = sorted(map(lambda (name, games): name, filter(lambda (name, games): games >= minimumGames, gamesPlayed.items())))
+
+print
+print("Players with at least " + str(minimumGames) + " games in tournament " + tournamentId + ":")
+print
+
+for name in names:
+    print(name)
